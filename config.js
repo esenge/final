58a4d43b1326d5e0ae941e289b8c4e1f03c5fb26
Agnese Skujiņa (ACN) <agnese.skujina@gmail.com>
2015-10-27 00:42:58 +0200

    adding first files

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'port': process.env.PORT || 8080,
+	'database': 'mongodb://esenge:aguc*020892@ds037244.mongolab.com:37244/mydatabase',
+	'secret': 'ilovescotchscotchyscotchscotch'
+};
