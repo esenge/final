804c0d2eea3a4fbc5aa54bb7cb903740ac2e6541
Agnese Skujiņa (ACN) <agnese.skujina@gmail.com>
2015-11-15 16:37:12 +0200

    comments

--- app/models/comment.js
+++ app/models/comment.js
@@ -1,13 +1,14 @@
-//var mongoose     = require('mongoose');
-//var Schema       = mongoose.Schema;
-//var bcrypt 		 = require('bcrypt-nodejs');
-//
-//// book schema 
-//var CommentSchema   = new Schema({
-//	comment: String,
-//	commentauthor: {type: Schema.ObjectId, ref: 'UserSchema', required: true },
-//	bookId: {type: Schema.ObjectId, ref: 'BookSchema', required: true },
-//	
-//});
-//
-//module.exports = mongoose.model('Comment', CommentSchema);
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+var bcrypt 		 = require('bcrypt-nodejs');
+
+// book schema 
+var CommentSchema   = new Schema({
+	comment: String,
+	commentauthor: {type: Schema.ObjectId, ref: 'UserSchema', required: true },
+	bookId: {type: Schema.ObjectId, ref: 'BookSchema', required: true },
+	dateCreated: { type: Date, default: Date.now }
+	
+});
+
+module.exports = mongoose.model('Comment', CommentSchema);
